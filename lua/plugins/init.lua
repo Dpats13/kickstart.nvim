6b064528424be77d42c73e207fe9f1e7f7c20711
Dan Patterson <dan@MacBook-Pro.local>
2025-03-30 13:41:32 -0400

    neotest + todo

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -46,6 +46,7 @@ require('lazy').setup({
   require 'plugins.harpoon',
   require 'plugins.avante',
   require 'plugins.dbtpal',
+  require 'plugins.neotest',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
---
+++ todo.md
@@ -0,0 +1,6 @@
+### ToDo:
+
+1. Get unit testing working 
+2. Get paste to not delete what's in buffer 
+3. Get auto complete to not work in certain files 
+
